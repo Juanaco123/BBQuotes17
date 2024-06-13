1d040f8cd85d0f8597aa085ceaf5960ff08b05bb
Juan Camilo Victoria <144855187+Juanaco123@users.noreply.github.com>
2024-06-13 09:47:40 -0500

    create models.

---
+++ BBQuotes17/Death.swift
@@ -0,0 +1,15 @@
+//
+//  Death.swift
+//  BBQuotes17
+//
+//  Created by Juan Camilo Victoria Pacheco on 13/06/24.
+//
+
+import Foundation
+
+struct Death: Decodable {
+    let character: String
+    let image: URL
+    let details: String
+    let lastWords: String
+}
---
+++ BBQuotes17/Quote.swift
@@ -0,0 +1,13 @@
+//
+//  Quote.swift
+//  BBQuotes17
+//
+//  Created by Juan Camilo Victoria Pacheco on 13/06/24.
+//
+
+import Foundation
+
+struct Quote: Decodable {
+    let quote: String
+    let character: String
+}

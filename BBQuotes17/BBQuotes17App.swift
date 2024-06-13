34f4d192a2f426a6db308e2bdf0d253eb3b05e57
Juan Camilo Victoria <144855187+Juanaco123@users.noreply.github.com>
2024-06-13 08:25:01 -0500

    Initial Commit

---
+++ BBQuotes17/BBQuotes17App.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotes17App.swift
+//  BBQuotes17
+//
+//  Created by Juan Camilo Victoria Pacheco on 13/06/24.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotes17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BBQuotes17/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BBQuotes17
+//
+//  Created by Juan Camilo Victoria Pacheco on 13/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
